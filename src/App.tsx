bb74368139a75bf7cd65a3f52a3831f5a196d09e
Bolaji Disu <abolajiidiisu@gmail.com>
2023-02-26 00:18:38 +0100

    completed HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './components/pages/LandingPage/homepage.components';
+import "./App.scss";
 
 
 function App() {
   return (
+    <div className="App">
     <Routes>
       <Route path='/home' element={<HomePage />} />
     </Routes>
+    </div>
   );
 }
 
