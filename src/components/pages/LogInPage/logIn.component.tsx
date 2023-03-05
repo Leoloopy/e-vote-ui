923eefd6abea9cbf04bca5886372cf59c0a250ed
Bolaji Disu <abolajiidiisu@gmail.com>
2023-03-05 22:48:34 +0100

    Wrapped Login and Registration Page with Page Container Component

--- src/components/pages/LogInPage/logIn.component.tsx
+++ src/components/pages/LogInPage/logIn.component.tsx
@@ -3,6 +3,7 @@ import Lottie from "lottie-react-web";
 import blackBallot from '../../../assets/lotties/blackBallot.json';
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import PageContainer from "../../reusables/pageContainer.component";
 
 
 const LogIn = () => {
@@ -15,36 +16,50 @@ const LogIn = () => {
           preserveAspectRatio: "xMidYMid slice"
         }
       };
-    return(
-        <>
-            <motion.div 
-                className="login"
-                initial={{opacity:0}}
-                animate={{opacity:1}}
-                exit={{opacity:0}}
-                transition={{duration:1}}
-                >
-                <div className="login_container">
-                    <Lottie 
-                        options={defaultOptions}
-                        height={200}
-                        width={200}
-                        style={{
-                            filter:"drop-shadow(0px 10px 10px #1d3ade)"
-                        }}
-                    />
-                    <form className="login_form">
-                        <label>Email Address</label>
-                        <input type="email" placeholder="acha@gmail.com" required/>
-                        <label>Password</label>
-                        <input type="password" placeholder="peterObi" required/>
-                        <p>Forget Password</p>
-                        <button>Log In</button>
-                        <p>Don't have an account? <Link to="/createAccount" style={{color:"white", textDecoration:"none", fontWeight:"bold"}}><span>SIGN UP</span></Link></p>
-                    </form>
-                </div>
-            </motion.div>       
-        </>
-    )
+    return (
+      <>
+        <PageContainer>
+          <motion.div
+            className="login"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+          >
+            <div className="login_container">
+              <Lottie
+                options={defaultOptions}
+                height={200}
+                width={200}
+                style={{
+                  filter: "drop-shadow(0px 10px 10px #1d3ade)",
+                }}
+              />
+              <form className="login_form">
+                <label>Email Address</label>
+                <input type="email" placeholder="acha@gmail.com" required />
+                <label>Password</label>
+                <input type="password" placeholder="peterObi" required />
+                <p>Forget Password</p>
+                <button>Log In</button>
+                <p>
+                  Don't have an account?{" "}
+                  <Link
+                    to="/createAccount"
+                    style={{
+                      color: "white",
+                      textDecoration: "none",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    <span>SIGN UP</span>
+                  </Link>
+                </p>
+              </form>
+            </div>
+          </motion.div>
+        </PageContainer>
+      </>
+    );
 }
 export default LogIn;
--- src/components/pages/Registration/createAccount.components.tsx
+++ src/components/pages/Registration/createAccount.components.tsx
@@ -1,44 +1,46 @@
 import "./createAccount.styles.scss";
 // import { useState } from "react";
 import { motion } from "framer-motion";
+import PageContainer from "../../reusables/pageContainer.component";
 const CreateAccount = () => {
-    return(
-        <>
-            <motion.div 
+    return (
+      <>
+        <PageContainer>
+          <motion.div
             className="register"
-                initial={{opacity:0}}
-                animate={{opacity:1}}
-                exit={{opacity:0}}
-                transition={{duration:1}}
-            >
-                <div className="register_container">
-                    <h1>Create Account</h1>
-                    <form className="register_form">
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+          >
+            <div className="register_container">
+              <h1>Create Account</h1>
+              <form className="register_form">
+                <input type="file" accept="image/*" multiple={false} required />
 
-                        <input type="file"  accept="image/*" multiple = {false} required/>
+                <input type="text" placeholder="First Name" required />
 
-                        <input type="text" placeholder="First Name" required/>
-                        
-                        <input type="text" placeholder="Last Name" required/>
-                        
-                        <input type="text" placeholder="+234 7053642897" required/>
-                        
-                        <input type="password" placeholder="peterObi" required/>
-                        
-                        <input type="text" placeholder="VoteWise.ng" required/>
-                        <select required>
-                            <option>Marketing</option>
-                            <option>IT</option>
-                            <option>Networking</option>
-                            <option>Account</option>
-                            <option>Sales</option>
-                        </select>
-                        <button>Create Account</button>
-                    </form>
-                </div>
-            </motion.div>       
-        </>
-    )
+                <input type="text" placeholder="Last Name" required />
+
+                <input type="text" placeholder="+234 7053642897" required />
+
+                <input type="password" placeholder="peterObi" required />
+
+                <input type="text" placeholder="VoteWise.ng" required />
+                <select required>
+                  <option>Marketing</option>
+                  <option>IT</option>
+                  <option>Networking</option>
+                  <option>Account</option>
+                  <option>Sales</option>
+                </select>
+                <button>Create Account</button>
+              </form>
+            </div>
+          </motion.div>
+        </PageContainer>
+      </>
+    );
 
 }
 
